c68efb11603ac6eb810cfd5b7e609c56b8d21abd
Shareef Fayed <shareef.fayed@gmail.com>
2021-01-14 18:06:01 +0200

    question 1

--- app/src/main/java/com/example/android/covid_19quiz/MainActivity.java
+++ app/src/main/java/com/example/android/covid_19quiz/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.android.covid_19quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submitButton = findViewById(R.id.submit);
+        submitButton.setOnClickListener(v -> submit(v));
+
+    }
+
+    protected void submit(View view) {
+        int score = 0;
+        // check question 1 answer
+        RadioGroup radioButtonGroup = findViewById(R.id.question_01_options);
+        int radioButtonID = radioButtonGroup.getCheckedRadioButtonId();
+        View radioButton = radioButtonGroup.findViewById(radioButtonID);
+        int idx = radioButtonGroup.indexOfChild(radioButton);
+        if (idx == 0) score++;
+
+        // show result
+        Toast.makeText(getApplicationContext(), "Your score: " + score + "/10", Toast.LENGTH_LONG).show();
     }
 }
